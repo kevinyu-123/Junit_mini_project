da830596c2b634df3b4ce1288f79a0f6333d20b2
hj <tntj123@naver.com>
2022-08-09 23:18:22 +0900

    initial commit

---
+++ src/main/java/site/mini/junitproject/JunitProjectApplication.java
@@ -0,0 +1,13 @@
+package site.mini.junitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/mini/junitproject/JunitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package site.mini.junitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
