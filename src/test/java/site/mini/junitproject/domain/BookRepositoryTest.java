65f298243c5d7a47ff7523b86ced6f1788671cc5
hj <tntj123@naver.com>
2022-08-19 23:35:29 +0900

    tests added

--- src/test/java/site/mini/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/mini/junitproject/domain/BookRepositoryTest.java
@@ -2,10 +2,13 @@ package site.mini.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import apple.laf.JRSUIState.TitleBarHeightState;
 import lombok.extern.slf4j.Slf4j;
 
 @DataJpaTest //DB 와 관련된 컴포넌트만 메모리에 로딩
@@ -29,15 +32,48 @@ public class BookRepositoryTest {
         //when (테스트 실행)
         Book bookPs = repository.save(book);
 
-
         //then (검증)
         assertEquals(title, bookPs.getTitle());
         assertEquals(author, bookPs.getAuthor());
+    } // 테스트 후  트렌젝션 종료
+
+    @Test
+    public void getBookList(){
+        String title = "junit5";
+        String author = "kevin";
+        
+        Book book = Book.builder()
+        .title(title)
+        .author(author)
+        .build();
+
+         repository.save(book);
+        List<Book> list = repository.findAll();
+
+        assertEquals(title, list.get(0).getTitle());
+        assertEquals(author, list.get(0).getAuthor());
     }
 
+    @Test
+    public void getOneBook(){
+
+        String title = "junit5";
+        String author = "kevin";
+        
+        Book book = Book.builder()
+        .title(title)
+        .author(author)
+        .build();
+
+        repository.save(book);
+
+        Book bookPs = repository.findById(1L).get();
+
+        assertEquals(title, bookPs.getTitle());
+        assertEquals(author, bookPs.getAuthor()); 
 
 
-
+    }
 
 
 
