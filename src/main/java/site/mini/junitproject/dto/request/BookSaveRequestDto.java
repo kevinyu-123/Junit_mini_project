40785c690691129e36b00f95efbe92327f12705d
hj <tntj123@naver.com>
2022-09-20 21:08:14 +0900

    validation 추가

--- src/main/java/site/mini/junitproject/dto/request/BookSaveRequestDto.java
+++ src/main/java/site/mini/junitproject/dto/request/BookSaveRequestDto.java
@@ -1,5 +1,8 @@
 package site.mini.junitproject.dto.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 import lombok.Setter;
 import site.mini.junitproject.domain.Book;
@@ -7,12 +10,19 @@ import site.mini.junitproject.domain.Book;
 @Data
 public class BookSaveRequestDto {
     
+    @NotBlank
+    @Size(min = 1, max = 50)
     private String title;
     
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String author;
 
     public Book toEntity(){
-        return Book.builder().title(title).author(author).build();
+        return Book.builder()
+            .title(title)
+            .author(author)
+            .build();
     }
 
 
--- src/main/java/site/mini/junitproject/web/BookApiController.java
+++ src/main/java/site/mini/junitproject/web/BookApiController.java
@@ -1,17 +1,26 @@
 package site.mini.junitproject.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import site.mini.junitproject.dto.request.BookSaveRequestDto;
 import site.mini.junitproject.dto.response.BookResponseDto;
 import site.mini.junitproject.dto.response.CmResponseDto;
 import site.mini.junitproject.service.BookService;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class BookApiController { // 컴포지션 = has 관계
@@ -19,12 +28,24 @@ public class BookApiController { // 컴포지션 = has 관계
     private final BookService service;
 
 
-    @GetMapping("api/v1/book")
-    public ResponseEntity<?> saveBook(@RequestBody BookSaveRequestDto saveDto){
+    @PostMapping("api/v1/book")
+    public ResponseEntity<?> saveBook(@RequestBody @Valid BookSaveRequestDto saveDto, BindingResult bindingResult){
+        log.error("bindingresult:" + bindingResult.hasErrors());
         BookResponseDto dto = service.saveBook(saveDto);
-        CmResponseDto<?> cmDto = CmResponseDto.builder().code(1).msg("save success").body(dto).build();
 
-        return new ResponseEntity<>(cmDto, HttpStatus.CREATED); // 201 = insert, 응답뿐만 아니라 바디데이터를 보내줘야 할 경우도 생김.
+        //차후 aop 처리
+        if(bindingResult.hasErrors()){
+            Map<String,String> errorMap = new HashMap<String,String>();
+            for(FieldError fe : bindingResult.getFieldErrors()){
+                errorMap.put(fe.getField(),fe.getDefaultMessage());
+            }
+            System.out.println(errorMap.toString());
+            log.info("errorMap: {}"+ errorMap.toString());
+
+            return new ResponseEntity<>(CmResponseDto.builder().code(2).msg(errorMap.toString()).body(dto).build(), HttpStatus.BAD_REQUEST); // 400 : 잘못된 요청
+        }
+
+        return new ResponseEntity<>(CmResponseDto.builder().code(1).msg("save success").body(dto).build(), HttpStatus.CREATED); // 201 = insert, 응답뿐만 아니라 바디데이터를 보내줘야 할 경우도 생김.
 
     }
 
